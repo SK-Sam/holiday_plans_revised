518462989fbe2bc471d897bbb931155c7f538677
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 07:53:50 -0600

    Adjust instance method test

--- holiday-plans/spec/models/manager_spec.rb
+++ holiday-plans/spec/models/manager_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Manager, type: :model do
   describe 'Relationships' do
     it { should have_many :workers }
   end
+  describe 'Instance Methods' do
+    it '#get_vacation_requests' do
+      manager = Manager.create
+      worker = manager.workers.create
+
+      worker.vacation_requests.create(start_date: Date.today + 20, end_date: Date.today + 25)
+      worker.vacation_requests.create(start_date: Date.today + 30, end_date: Date.today + 37)
+      worker.vacation_requests.create(start_date: Date.today + 24, end_date: Date.today + 28)
+
+      expect(manager.get_vacation_requests(nil).size).to eq(3)
+      expect(manager.get_vacation_requests(nil).first).to be_a(VacationRequest)
+
+      expect(manager.get_vacation_requests("pending").size).to eq(3)
+      expect(manager.get_vacation_requests("approved").size).to eq(0)
+    end
+  end
 end
