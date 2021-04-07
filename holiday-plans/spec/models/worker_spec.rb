0d2cc1c9f807b79ebd93e8b139199736c49b0430
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 05:25:40 -0600

    Create test for worker model

---
+++ holiday-plans/spec/models/worker_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Worker, type: :model do
+  describe 'Attributes' do
+    it 'can instantiate with attributes' do
+      manager = Manager.create
+      worker = manager.workers.create
+
+      expect(worker.vacation_time_remaining).to eq(28)
+      expect(worker.requests_remaining).to eq(30)
+      expect(worker.manager_id).to eq(manager.id)
+    end
+  end
+end
