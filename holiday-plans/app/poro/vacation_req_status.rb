957e5bde564566890f2059d4a4dbec1dec65a5e2
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 07:11:59 -0600

    Change structure to utilize DB object better

--- holiday-plans/app/poro/vacation_req_status.rb
+++ holiday-plans/app/poro/vacation_req_status.rb
@@ -7,11 +7,11 @@ class VacationReqStatus
               :vacation_start_date,
               :vacation_end_date
 
-  def initialize(vacation_request, manager_id=nil)
+  def initialize(vacation_request)
     @id = vacation_request.id
     @author = vacation_request.worker_id
     @status = vacation_request.status
-    @resolved_by = manager_id
+    @resolved_by = vacation_request.resolved_by
     @request_created_at = vacation_request.created_at.iso8601
     @vacation_start_date = vacation_request.start_date.iso8601
     @vacation_end_date = vacation_request.end_date.iso8601
