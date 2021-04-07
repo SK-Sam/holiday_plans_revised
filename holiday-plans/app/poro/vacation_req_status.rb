f1417b94d2a88346b92fc46658e0c379a3e6248f
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 06:39:28 -0600

    Change var name to be more specific

--- holiday-plans/app/poro/vacation_req_status.rb
+++ holiday-plans/app/poro/vacation_req_status.rb
@@ -7,11 +7,11 @@ class VacationReqStatus
               :vacation_start_date,
               :vacation_end_date
 
-  def initialize(vacation_request, manager=nil)
+  def initialize(vacation_request, manager_id=nil)
     @id = vacation_request.id
     @author = vacation_request.worker_id
     @status = vacation_request.status
-    @resolved_by = manager
+    @resolved_by = manager_id
     @request_created_at = vacation_request.created_at.iso8601
     @vacation_start_date = vacation_request.start_date.iso8601
     @vacation_end_date = vacation_request.end_date.iso8601
