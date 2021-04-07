76953fc6603b094ffd6d06ef08cf50ba2eb74702
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 06:11:33 -0600

    Create route for second endpoint

--- holiday-plans/config/routes.rb
+++ holiday-plans/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       get '/workers/:id/vacation_requests', to: 'workers#vacation_requests'
+      get'/workers/:id/vacation_days', to: 'workers#vacation_days'
     end
   end
 end
