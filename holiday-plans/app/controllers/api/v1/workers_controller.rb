5761b5c662a8312639cd9d41face1ab7311e1e50
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 06:27:20 -0600

    Refactor and create vacation_days action using facades

--- holiday-plans/app/controllers/api/v1/workers_controller.rb
+++ holiday-plans/app/controllers/api/v1/workers_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::WorkersController < ApplicationController
 
   def vacation_requests
-    worker = Worker.find(params[:id])
-    list_of_requests = worker.get_vacation_requests(params[:status])
-    list_of_statuses = list_of_requests.map do |request|
-      VacationReqStatus.new(request)
-    end
-    filtered_requests_list = FilteredVacationReqs.new(list_of_statuses)
-    render json: WorkerRequestsSerializer.new(filtered_requests_list)
+    filtered_workers_request_list = WorkersFacade.filter_worker_vacation_requests(params[:id], params[:status])
+    render json: WorkerRequestsSerializer.new(filtered_workers_request_list)
+  end
+
+  def vacation_days
+    render json: RemainingVacationDaysSerializer.new(
+      WorkersFacade.get_remaining_vacation_days(params[:id])
+    )
   end
 end
