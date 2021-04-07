bc4c8057b67c16d9e1368d5307f359d5fd709da8
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 06:48:07 -0600

    Create new POST route

--- holiday-plans/config/routes.rb
+++ holiday-plans/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace 'v1' do
       get '/workers/:id/vacation_requests', to: 'workers#vacation_requests'
       get'/workers/:id/vacation_days', to: 'workers#vacation_days'
+      post '/workers/:id/requests', to: 'workers#create_vacation_request'
     end
   end
 end
