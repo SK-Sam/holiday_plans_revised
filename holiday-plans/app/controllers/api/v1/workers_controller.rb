ef985facc6a3f2ac8b67d0b361c8dacc2a8a8a2a
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 07:04:14 -0600

    Complete endpoint which passes tests

--- holiday-plans/app/controllers/api/v1/workers_controller.rb
+++ holiday-plans/app/controllers/api/v1/workers_controller.rb
@@ -10,4 +10,16 @@ class Api::V1::WorkersController < ApplicationController
       WorkersFacade.get_remaining_vacation_days(params[:id])
     )
   end
+
+  def create_vacation_request
+    vacation_request = WorkersFacade.create_worker_vacation_request(
+      params[:id], params[:start_date], params[:end_date]
+    )
+    if vacation_request.save
+      vacation_request_status = VacationReqStatus.new(vacation_request)
+      render json: VacationRequestSerializer.new(vacation_request_status), status: :created
+    else
+      render json: {error: "Please check start and end date to see if they're valid."}, status: 409
+    end
+  end
 end
