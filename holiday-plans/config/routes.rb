e7dd35f8a4265632878141d821e68f83dceaab8c
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 07:24:08 -0600

    Create route for Manager vacation request route

--- holiday-plans/config/routes.rb
+++ holiday-plans/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       get '/workers/:id/vacation_requests', to: 'workers#vacation_requests'
       get'/workers/:id/vacation_days', to: 'workers#vacation_days'
       post '/workers/:id/requests', to: 'workers#create_vacation_request'
+
+      get '/managers/:id/vacation_requests', to: 'managers#vacation_requests'
     end
   end
 end
