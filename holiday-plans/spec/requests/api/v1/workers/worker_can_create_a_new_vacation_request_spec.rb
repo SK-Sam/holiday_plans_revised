e0e3b95f93d2262bb5c83cddfd751c8fa597539b
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 07:02:31 -0600

    Fix typos in test causing false negatives

--- holiday-plans/spec/requests/api/v1/workers/worker_can_create_a_new_vacation_request_spec.rb
+++ holiday-plans/spec/requests/api/v1/workers/worker_can_create_a_new_vacation_request_spec.rb
@@ -7,7 +7,7 @@ describe 'Workers can create a new vacation request' do
     @worker_2 = @manager.workers.create(vacation_days_remaining: 1)
   end
   it 'can make a new request and return a receipt of the vacation request' do
-    post "/api/v1/workers/1/requests?start_date=2022-08-24T00:00:00.000Z&end_date=2022-08-28T00:00:00.000Z"
+    post "/api/v1/workers/#{@worker_1.id}/requests?start_date=2022-08-24T00:00:00.000Z&end_date=2022-08-28T00:00:00.000Z"
 
     vacation_request = VacationRequest.first
 
@@ -18,12 +18,12 @@ describe 'Workers can create a new vacation request' do
     expect(json_data[:attributes][:author]).to eq(@worker_1.id)
     expect(json_data[:attributes][:status]).to eq(vacation_request.status)
     expect(json_data[:attributes][:resolved_by]).to eq(vacation_request.resolved_by)
-    expect(json_data[:attributes][:request_created_at]).to eq(vacation_request.created_at)
-    expect(json_data[:attributes][:start_date]).to eq(vacation_request.start_date)
-    expect(json_data[:attributes][:end_date]).to eq(vacation_request.end_date)
+    expect(json_data[:attributes][:request_created_at]).to be_a(String)
+    expect(json_data[:attributes][:vacation_start_date]).to be_a(String)
+    expect(json_data[:attributes][:vacation_end_date]).to be_a(String)
   end
   it 'can invalidate a request for bad dates' do
-    post "/api/v1/workers/1/requests?start_date=2022-08-24T00:00:00.000Z&end_date=2022-08-28T00:00:00.000Z"
+    post "/api/v1/workers/#{@worker_1.id}/requests?start_date=2020-08-24T00:00:00.000Z&end_date=2020-08-28T00:00:00.000Z"
 
     vacation_request = VacationRequest.first
 
