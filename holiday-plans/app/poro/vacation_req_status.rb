a800255479ee7c7990d8d80654b24f4d627376b9
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 05:53:37 -0600

    Create poro to match JSON contract for serializer to use

---
+++ holiday-plans/app/poro/vacation_req_status.rb
@@ -0,0 +1,19 @@
+class VacationReqStatus
+  attr_reader :id,
+              :author,
+              :status,
+              :resolved_by,
+              :request_created_at,
+              :vacation_start_date,
+              :vacation_end_date
+
+  def initialize(vacation_request, manager=nil)
+    @id = vacation_request.id
+    @author = vacation_request.worker_id
+    @status = vacation_request.status
+    @resolved_by = manager
+    @request_created_at = vacation_request.created_at.iso8601
+    @vacation_start_date = vacation_request.start_date.iso8601
+    @vacation_end_date = vacation_request.end_date.iso8601
+  end
+end
