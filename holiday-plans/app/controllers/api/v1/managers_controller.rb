7c6ebfd7a3ddd223c6bf1c3b603a1ea4f6794735
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 08:35:38 -0600

    Create update action for vacation request via manager action

--- holiday-plans/app/controllers/api/v1/managers_controller.rb
+++ holiday-plans/app/controllers/api/v1/managers_controller.rb
@@ -10,4 +10,12 @@ class Api::V1::ManagersController < ApplicationController
     worker_data = WorkerData.new(worker)
     render json: WorkerOverviewSerializer.new(worker_data)
   end
+
+  def approve_vacation_request
+    vacation_request = VacationRequest.find(params[:vacation_request_id])
+    manager = Manager.find(params[:id])
+    vacation_request.update(status: params[:status], resolved_by: manager.id)
+
+    render json: {message: "Status updated to #{vacation_request.status}"}, status: 204
+  end
 end
