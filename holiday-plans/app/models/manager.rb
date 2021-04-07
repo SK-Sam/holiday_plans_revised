165472df517888121931b27ac79e22c5aa02991e
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 07:51:12 -0600

    Create get_vacation_requests method to get all requests under a manager

--- holiday-plans/app/models/manager.rb
+++ holiday-plans/app/models/manager.rb
@@ -1,3 +1,15 @@
 class Manager < ApplicationRecord
   has_many :workers
+
+  def get_vacation_requests(status)
+    return VacationRequest.joins(worker: :vacation_requests)
+    .where("workers.manager_id = ?", self.id)
+    .order("created_at ASC") if status == nil || status == "rejected" || status == ""
+
+    VacationRequest.joins(worker: :vacation_requests)
+    .where("workers.manager_id = ?", self.id)
+    .where("vacation_requests.status = ?", status)
+    .order("created_at ASC")
+    .order("created_at ASC")
+  end
 end
