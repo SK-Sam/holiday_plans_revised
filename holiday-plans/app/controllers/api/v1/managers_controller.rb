427ca12691465565d4cc0be2fcd575b0bdd52a64
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 07:58:15 -0600

    Create controller action which passes tests for first Manager endpoint

--- holiday-plans/app/controllers/api/v1/managers_controller.rb
+++ holiday-plans/app/controllers/api/v1/managers_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::ManagersController < ApplicationController
-
+  
+  def vacation_requests
+    filtered_workers_vacation_requests = ManagersFacade.filter_workers_vacation_requests(params[:id], params[:status])
+    render json: WorkerRequestsSerializer.new(filtered_workers_vacation_requests)
+  end
 end
