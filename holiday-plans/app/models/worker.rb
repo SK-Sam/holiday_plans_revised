a176d4dd50559f192745ded1564fc879feed0452
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 06:09:43 -0600

    Complete get_vacation_requests method which passes tests

--- holiday-plans/app/models/worker.rb
+++ holiday-plans/app/models/worker.rb
@@ -1,4 +1,9 @@
 class Worker < ApplicationRecord
   belongs_to :manager
   has_many :vacation_requests
+
+  def get_vacation_requests(status)
+    return vacation_requests.order("created_at ASC") if status == nil || status == ""
+    vacation_requests.where("status = ?", status).order("created_at ASC")
+  end
 end
