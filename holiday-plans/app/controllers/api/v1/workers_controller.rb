46293e75ff11ff1470d57b1f3dda8abd6950cb0b
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 06:10:07 -0600

    Complete first endpoint

--- holiday-plans/app/controllers/api/v1/workers_controller.rb
+++ holiday-plans/app/controllers/api/v1/workers_controller.rb
@@ -1,3 +1,12 @@
 class Api::V1::WorkersController < ApplicationController
 
+  def vacation_requests
+    worker = Worker.find(params[:id])
+    list_of_requests = worker.get_vacation_requests(params[:status])
+    list_of_statuses = list_of_requests.map do |request|
+      VacationReqStatus.new(request)
+    end
+    filtered_requests_list = FilteredVacationReqs.new(list_of_statuses)
+    render json: WorkerRequestsSerializer.new(filtered_requests_list)
+  end
 end
