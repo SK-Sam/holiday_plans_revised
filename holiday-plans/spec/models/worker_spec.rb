e79b322a78ad90ce01fa21b4230619303c51e8a0
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 06:07:14 -0600

    Create tests for method get_vacation_requests

--- holiday-plans/spec/models/worker_spec.rb
+++ holiday-plans/spec/models/worker_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe Worker, type: :model do
       expect(worker.manager_id).to eq(manager.id)
     end
   end
+
+  describe 'Instance Method Tests' do
+    it '#get_vacation_requests' do
+      manager = Manager.create
+      worker = manager.workers.create
+
+      worker.vacation_requests.create(start_date: Date.today + 20, end_date: Date.today + 25)
+      worker.vacation_requests.create(start_date: Date.today + 30, end_date: Date.today + 37)
+      worker.vacation_requests.create(start_date: Date.today + 24, end_date: Date.today + 28)
+
+      expect(worker.get_vacation_requests(nil).size).to eq(3)
+      expect(worker.get_vacation_requests(nil).first).to be_a(VacationRequest)
+
+      expect(worker.get_vacation_requests("pending").size).to eq(3)
+      expect(worker.get_vacation_requests("approved").size).to eq(0)
+    end
+  end
 end
