da95e5b144eee18aab2903c117933a6ad8c683b0
Sam Yeo <yeosamuel95@gmail.com>
2021-04-07 07:54:11 -0600

    Adjust query to avoid duplicate rows

--- holiday-plans/app/models/manager.rb
+++ holiday-plans/app/models/manager.rb
@@ -4,12 +4,14 @@ class Manager < ApplicationRecord
   def get_vacation_requests(status)
     return VacationRequest.joins(worker: :vacation_requests)
     .where("workers.manager_id = ?", self.id)
-    .order("created_at ASC") if status == nil || status == "rejected" || status == ""
+    .order("created_at ASC")
+    .distinct if status == nil || status == "rejected" || status == ""
 
     VacationRequest.joins(worker: :vacation_requests)
     .where("workers.manager_id = ?", self.id)
     .where("vacation_requests.status = ?", status)
     .order("created_at ASC")
     .order("created_at ASC")
+    .distinct
   end
 end
